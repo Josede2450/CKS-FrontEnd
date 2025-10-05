de5e87933f21c36d4c7a931a4c28087e1e1a7fe0
Jose De Jesus <josegabrieldejesus@hotmail.com>
2025-10-05 12:15:39 -0400

    Ready to deploy 19

--- app/components/admin/ServicesManager.tsx
+++ app/components/admin/ServicesManager.tsx
@@ -3,10 +3,10 @@
 
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import dynamic from "next/dynamic";
-import "react-quill/dist/quill.snow.css";
+import "react-quill-new/dist/quill.snow.css";
 import { fetchWithCsrf } from "../../lib/fetchWithCsrf";
 
-const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
 
 /* =========================
    Types aligned to backend
